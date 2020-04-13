e9c397f4780705a6e8fc91ce5b4daebb4e87548f
Devone Austin <devone.austin@gmail.com>
2020-04-13 23:10:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  return "Squawk!" 
+  phrase = "Pretty bird!"
+  puts phrase
+  return "Pretty bird!"
+end
+
